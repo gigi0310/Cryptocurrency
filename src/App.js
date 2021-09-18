bc6a1e100d16b5449294fc92462a0560ee1705b0
Karen cai <gigi.cai0310@gmail.com>
2021-09-18 14:40:08 +1000

    initialize project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 import './App.css';
+import Coin from "./Coin";
+
+
 
 function App() {
+
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() =>{
+    const fetchData = async() =>{
+      try {
+        const res = await axios.get ('https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=100&page=1&sparkline=false') 
+
+        setCoins(res.data);
+        console.log(res.data);
+      } catch (err) {
+        alert("There is an error");
+      }
+    }
+    fetchData()
+}, [])  
+
+  const handleChange = e =>{
+    setSearch(e.target.value)
+  }
+
+  const filteredCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLocaleLowerCase()) || coin.symbol.toLowerCase().includes(search.toLocaleLowerCase())
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Search a currency</h1>
+        <form>
+          <input type="text" className="coin-input" placeholder="Search" onChange={handleChange} />
+        </form>
+      </div>
+      {filteredCoins.map(coin =>{
+        return (
+          <Coin 
+            key={coin.id} 
+            rank = {coin.market_cap_rank}
+            name={coin.name} 
+            image={coin.image} 
+            symbol={coin.symbol} 
+            marketcap={coin.market_cap}
+            price = {coin.current_price}
+            priceChange = {coin.price_change_percentage_24h}
+            volume = {coin.total_volume}
+          />);
+        
+      })}
     </div>
+
   );
 }
 
